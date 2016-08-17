f924f02e4307716192cf6dabbff1cac313abac8b
herzon <itsystemsflores@gmail.com>
2016-08-17 11:38:12 -0700

    added home and angular project

--- SummarizerSite/SummarizerSite/settings.py
+++ SummarizerSite/SummarizerSite/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corpus_summary'
+    'corpus_summary',
+    'authentication',
+    'home'
 ]
 
 MIDDLEWARE = [
